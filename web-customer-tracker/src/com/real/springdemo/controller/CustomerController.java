4420e1bf42141cbf272c50aedcb9e561194599e2
Erthax <mateuszkochanek01@gmail.com>
2019-09-25 20:44:20 +0200

    #004.Customer Controller adds customers to the model.

--- web-customer-tracker/src/com/real/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/real/springdemo/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package com.real.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.real.springdemo.dao.CustomerDAO;
+import com.real.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from the dao
+		List<Customer> customers = customerDAO.getCustomer();
+		
+		//add the customers to the model
+		theModel.addAttribute("customers",customers);
+		
 		return "list-customers";
 	}
 	
