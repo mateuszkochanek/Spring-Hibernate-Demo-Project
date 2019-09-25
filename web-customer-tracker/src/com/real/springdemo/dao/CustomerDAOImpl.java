565f9353b69455b976388d89149d91398a4f1f20
Erthax <mateuszkochanek01@gmail.com>
2019-09-25 20:26:04 +0200

    #003.Customer DAO implementation created

---
+++ web-customer-tracker/src/com/real/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,34 @@
+package com.real.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.real.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	//need to inject session factory bean
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomer() {
+		
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query<Customer> query = session.createQuery("from customer",Customer.class);
+		
+		List<Customer> customers = query.getResultList();
+		
+		return customers;
+	}
+
+}
