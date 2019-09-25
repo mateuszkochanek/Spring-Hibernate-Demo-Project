9cd686ce32a124b87197d075715dacce781d3178
Erthax <mateuszkochanek01@gmail.com>
2019-09-25 20:17:53 +0200

    #002.Customer Data Access Object created

---
+++ web-customer-tracker/src/com/real/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.real.springdemo.dao;
+
+import java.util.List;
+
+import com.real.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomer();
+}
