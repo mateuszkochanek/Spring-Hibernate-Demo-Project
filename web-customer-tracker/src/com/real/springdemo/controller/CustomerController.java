cf5a8f20e18ffd567cb84d1e3226e83adef75523
Erthax <mateuszkochanek01@gmail.com>
2019-10-03 16:45:30 +0200

    #011.Customer from form saved in database

--- web-customer-tracker/src/com/real/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/real/springdemo/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.real.springdemo.dao.CustomerDAO;
@@ -41,5 +43,13 @@ public class CustomerController {
 		return "customer-form";
 	}
 	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		
+		return "redirect:/customer/list";
+	}
 	
 }
--- web-customer-tracker/src/com/real/springdemo/service/CustomerServiceImpl.java
+++ web-customer-tracker/src/com/real/springdemo/service/CustomerServiceImpl.java
@@ -24,4 +24,12 @@ public class CustomerServiceImpl implements CustomerService {
 		return customerDAO.getCustomers();
 	}
 
+
+	@Override
+	@Transactional
+	public void saveCustomer(Customer theCustomer) {
+		
+		customerDAO.saveCustomer(theCustomer);
+	}
+
 }
