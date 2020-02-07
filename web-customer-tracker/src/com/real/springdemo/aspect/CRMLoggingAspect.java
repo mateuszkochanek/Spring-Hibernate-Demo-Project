1414a95319b28e0b8c24640dacd508a7edfa4408
Erthax <mateuszkochanek01@gmail.com>
2020-02-07 21:37:59 +0100

    #020.AOP Logs Added

--- web-customer-tracker/src/com/real/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker/src/com/real/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,10 @@
 package com.real.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import com.sun.istack.logging.Logger;
@@ -9,6 +13,39 @@ import com.sun.istack.logging.Logger;
 @Component
 public class CRMLoggingAspect {
 	
-	private Logger myLogger = Logger.getLogger(getClass().getName(), null);
+	private Logger myLogger = Logger.getLogger(CRMLoggingAspect.class.getName(), CRMLoggingAspect.class.getClass());
 
+	@Pointcut("execution(* com.real.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.real.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.real.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		String theMethod = theJoinPoint.getSignature().toString();
+		myLogger.info("======> Before Advice before method:" + theMethod);
+		
+		Object[] args = theJoinPoint.getArgs();
+		for(Object tempArg : args) {
+			myLogger.info("======> Argument: )" + tempArg);
+		}
+	}
+	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		String theMethod = theJoinPoint.getSignature().toString();
+		myLogger.info("======> After Returning before method:" + theMethod);
+
+		myLogger.info("======> Argument: )" + theResult);
+	}
 }
